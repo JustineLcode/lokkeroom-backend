9fe7f2110f1bec28d499acc250775f86e8642ed6
Ibrahim <poyrazibrahim270@gmail.com>
2025-01-29 11:33:35 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+require("./src/db/dbConnection")
+const port = process.env.PORT || 5001
+const router = require("./src/routers")
+
+//Middelwares
+app.use(express.json())
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}))
+
+
+app.use("/api", router)
+
+
+app.get("/", (req, res) => {
+    res.json({
+     message: "Bienvenue"
+    })
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Le server fonctionne sur le port ${port}`);
+    
+})
